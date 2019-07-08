92ffc5b018595599daa718d527a0046cea0cf66b
Alen Lukic <alen@amplifydmusic.com>
2019-07-07 23:31:04 -0700

    Package structure. Config file update. First draft of ingestion API.

---
+++ src/api/iex_api.py
@@ -0,0 +1,160 @@
+import logging
+import json
+import requests
+from enum import Enum
+from os.path import join
+
+from src.utils.api_utils import build_response_context
+
+
+CONFIG = json.load(open('../../config.json', 'r'))
+LOG_FILE = join(CONFIG['LOG_DIRECTORY'], 'src.api.iex_api')
+PROCESSED_DATA_DIR = join(CONFIG['DATA_DIRECTORY'], 'processed')
+
+logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG)
+
+
+class Endpoint(Enum):
+    """ Enumeration of API endpoints used in stock data ingestion. """
+
+    ADVANCED_STATS = '%s/advanced-stats'
+    CASH_FLOW = '%s/cash-flow'
+    DIVIDENDS = '%s/dividends/%s'
+    EARNINGS = '%s/earnings/%s'
+    FINANCIALS = '%s/financials'
+    HISTORICAL_PRICES = '%s/chart/%s'
+    INCOME = '%s/income'
+
+
+class IEXCloudAPI:
+    """ Class encapsulating the IEX Cloud API. """
+
+    def __init__(self, api_key):
+        """ Initializes class with API's base url and parameters (API token).
+
+        Parameters:
+            api_key (string): public API token needed to access the API.
+        """
+
+        self.base_url = 'https://cloud.iexapis.com/stable/stock'
+        self.params = {'token': api_key}
+
+    def get_advanced_stats(self, ticker):
+        """ Make GET request to the /advanced-stats endpoint.
+
+        Parameters:
+            ticker (string): Ticker symbol for which to pull data.
+        """
+
+        request_url = join(self.base_url, Endpoint.ADVANCED_STATS.value % ticker)
+        return self._get_response(request_url)
+
+    def get_cash_flow(self, ticker):
+        """ Make GET request to the /cash-flow endpoint.
+
+        Parameters:
+            ticker (string): Ticker symbol for which to pull data.
+        """
+
+        request_url = join(self.base_url, Endpoint.CASH_FLOW.value % ticker)
+        return self._get_response(request_url)
+
+    def get_dividends(self, ticker, time_range='1y'):
+        """ Make GET request to the /dividends endpoint.
+
+        Parameters:
+            ticker (string): Ticker symbol for which to pull data.
+            time_range (string): Period for which to pull data.
+        """
+
+        request_url = join(self.base_url, Endpoint.DIVIDENDS.value % (ticker, time_range))
+        return self._get_response(request_url)
+
+    def get_earnings(self, ticker, quarters='4'):
+        """ Make GET request to the /earnings endpoint.
+
+        Parameters:
+            ticker (string): Ticker symbol for which to pull data.
+            quarters (string): Number of quarters for which to pull data.
+        """
+
+        request_url = join(self.base_url, Endpoint.EARNINGS.value % (ticker, quarters))
+        return self._get_response(request_url)
+
+    def get_financials(self, ticker):
+        """ Make GET request to the /financials endpoint.
+
+        Parameters:
+            ticker (string): Ticker symbol for which to pull data.
+        """
+
+        request_url = join(self.base_url, Endpoint.FINANCIALS.value % ticker)
+        return self._get_response(request_url)
+
+    def get_historical_prices(self, ticker, time_range='1y'):
+        """ Make GET request to the /chart endpoint.
+
+        Parameters:
+            ticker (string): Ticker symbol for which to pull data.
+            time_range (string): Period for which to pull data.
+        """
+
+        request_url = join(self.base_url, Endpoint.HISTORICAL_PRICES.value % (ticker, time_range))
+        return self._get_response(request_url)
+
+    def get_income(self, ticker):
+        """ Make GET request to the /income endpoint.
+
+        Parameters:
+            ticker (string): Ticker symbol for which to pull data.
+        """
+
+        request_url = join(self.base_url, Endpoint.INCOME.value % ticker)
+        return self._get_response(request_url)
+
+    def _get_response(self, request_url):
+        """ Make GET request using the given URL, handle any errors, and return response content.
+
+        Parameters:
+            request_url (string): URL for which to make GET request.
+        """
+
+        try:
+            response = requests.get(request_url, params=self.params)
+            if response.status_code != 200:
+                response_context = build_response_context(response)
+                logging.warning('Got non-200 response while hitting %s: %s', request_url, json.dumps(response_context))
+                return {}
+            return response.content
+        except Exception as e:
+            logging.warning('The following exception occurred while hitting %s: %s', request_url, str(e))
+            return {}
+
+
+def ingest_stock_data():
+    """ Ingests financial and technical indicator data for all actively traded stocks on NASDAQ. """
+
+    input_path = join(PROCESSED_DATA_DIR, 'all_tickers.txt')
+    output_path = join(PROCESSED_DATA_DIR, 'stock_data.json')
+
+    with open(input_path, 'r') as input_file, open(output_path, 'w') as output_file:
+        all_tickers = [t.strip() for t in input_file.readlines()]
+        api = IEXCloudAPI(CONFIG['API_KEY'])
+        all_data = {}
+
+        for ticker in all_tickers:
+            all_data[ticker] = {
+                Endpoint.ADVANCED_STATS.name: api.get_advanced_stats(ticker),
+                Endpoint.CASH_FLOW.name: api.get_cash_flow(ticker),
+                Endpoint.DIVIDENDS.name: api.get_dividends(ticker),
+                Endpoint.EARNINGS.name: api.get_earnings(ticker),
+                Endpoint.FINANCIALS.name: api.get_financials(ticker),
+                Endpoint.HISTORICAL_PRICES.name: api.get_historical_prices(ticker),
+                Endpoint.INCOME.name: api.get_income(ticker)
+            }
+
+        json.dump(all_data, output_file, indent=2, sort_keys=True)
+
+
+if __name__ == '__main__':
+    ingest_stock_data()
--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -1,23 +1,23 @@
 import logging
+import json
 from os.path import join
 
-LOG_FILE = '../../logs/src.utils.data_utils'
-PROCESSED_DATA_DIR = '../../data/processed'
-RAW_DATA_DIR = '../../data/raw'
+CONFIG = json.load(open('../../config.json', 'r'))
+LOG_FILE = join(CONFIG['LOG_DIRECTORY'], 'src.utils.data_utils')
+PROCESSED_DATA_DIR = join(CONFIG['DATA_DIRECTORY'], 'processed')
+RAW_DATA_DIR = join(CONFIG['DATA_DIRECTORY'], 'raw')
 
 logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG)
 
 
-""" Uses raw dump from NASDAQ's FTP server to generate a list containing all tickers traded on the exchange.
-
-Parameters:
-input_name (string): raw dump file name, located in the raw data directory
-output_name (string): output file name - will be located in the processed data directory
-
-"""
-
-
 def create_stock_list(input_name='nasdaq_stock_dump.txt', output_name='all_tickers.txt'):
+    """ Uses raw dump from NASDAQ's FTP server to generate a list containing all tickers traded on the exchange.
+
+    Parameters:
+        input_name (string): raw dump file name, located in the raw data directory
+        output_name (string): output file name - will be located in the processed data directory
+    """
+
     input_path = join(RAW_DATA_DIR, input_name)
     output_path = join(PROCESSED_DATA_DIR, output_name)
 
@@ -38,5 +38,5 @@ def create_stock_list(input_name='nasdaq_stock_dump.txt', output_name='all_ticke
         for row, error in errors:
             logging.warning('The following error occurred while processing row %s: %s', row, error)
 
-        output_file.write('\n'.join(tickers))
-
+        sorted_tickers = sorted(list(tickers))
+        output_file.write('\n'.join(sorted_tickers))
